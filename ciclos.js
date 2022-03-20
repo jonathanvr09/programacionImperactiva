53939b41cb3b9e45b3e7bdd60e5bd1f61fd7090e
Jonathan Vargas Ríos <89598945+jonathanvr09@users.noreply.github.com>
2022-03-20 17:45:08 -0500

    Se agregan nuevos cambios 20/03/22

---
+++ ciclos.js
@@ -0,0 +1,18 @@
+// for (let num=5; num<=20; num++) 
+// {
+//   if (num%5==1)
+//    {
+//        console.log(num)
+//        num=num+3
+     
+//    }
+// }
+
+// let dameCinco = ()=> [ a, b, c, d, e];
+
+// console.log(dameCinco(1, 2, 3, 4, 5))
+
+
+for( let loro = 1; loro <= 5; loro++ ){
+  console.log('Loro habla' + loro);
+};
